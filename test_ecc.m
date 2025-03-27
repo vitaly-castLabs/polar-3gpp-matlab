4a2c77d17dd97385a1e3bea5ffe5e7015798b1eb
Vitaly Ivanov <vitaly.ivanov@castlabs.com>
2025-03-27 17:34:05 +0700

    more advanced test added

--- test_ecc.m
+++ test_ecc.m
@@ -3,12 +3,12 @@ function test_ecc() %#codegen
     a = [1 1 1 1 0 0 0 0 1 1 1 1 0 0 0 0];
     G = 32; % codeword length, has to be power of 2, more than length(a)
 
-    disp('Org message:');
+    disp("Org message:");
     disp(a);
 
     % Encode
     f = PUCCH_encoder(a, G);
-    disp('Encoded:');
+    disp("Encoded:");
     disp(f);
 
     f_corrupted = f;
@@ -21,7 +21,7 @@ function test_ecc() %#codegen
         f_corrupted(idx) = mod(f(idx) + 1, 2);
     end
 
-    disp('Corrupted (3 bits):');
+    disp("Corrupted:");
     disp(f_corrupted);
 
     % Convert to LLR (ln (p0/p1)), that is what decoder expects
@@ -32,14 +32,83 @@ function test_ecc() %#codegen
     min_sum = true; % Use log-sum-product for better performance
     decoded_a = PUCCH_decoder(f_tilde, length(a), L, min_sum);
 
-    % Print decoded result
-    disp('Decoded:');
+    disp("Decoded:");
     disp(decoded_a);
 
-    % Check if decoding was successful
+    disp("Test #1 (3 bit flips, burst):");
     if isequal(a, decoded_a)
-        disp('Decoding successful!');
+        disp("success" + newline);
     else
-        disp('Decoding failed!');
+        disp("failure" + newline);
+    end
+
+    % Test #2 - 3 flips with gaps
+    f_corrupted = f;
+    flips = [2, 4, 6];
+    for i = 1:length(flips)
+        idx = flips(i);
+        f_corrupted(idx) = mod(f(idx) + 1, 2);
+    end
+
+    f_tilde = bits_to_llr(f_corrupted);
+    decoded_a = PUCCH_decoder(f_tilde, length(a), L, min_sum);
+    disp("Test #2 (3 bit flips, spread):");
+    if isequal(a, decoded_a)
+        disp("success" + newline);
+    else
+        disp("failure" + newline);
+    end
+
+    % Test #3 - 4 flips
+    f_corrupted = f;
+    flips = [1, 3, 5, 7];
+    for i = 1:length(flips)
+        idx = flips(i);
+        f_corrupted(idx) = mod(f(idx) + 1, 2);
+    end
+
+    f_tilde = bits_to_llr(f_corrupted);
+    decoded_a = PUCCH_decoder(f_tilde, length(a), L, min_sum);
+    disp("Test #3 (4 bit flips, spread):");
+    if isempty(decoded_a)
+        disp("success (decoding failed as expected)" + newline);
+    else
+        disp("failure - decoded:");
+        disp(decoded_a);
+        disp();
+    end
+
+    % Test #4 - 4 erasures
+    f_tilde = bits_to_llr(f);
+
+    erasures = [5, 10, 15, 20];
+    for i = 1:length(erasures)
+        idx = erasures(i);
+        f_tilde(idx) = 0; % setting it to 0 indicates equal probs for 0 and 1
+    end
+
+    decoded_a = PUCCH_decoder(f_tilde, length(a), L, min_sum);
+    disp("Test #4 (4 erasures, spread):");
+    if isequal(a, decoded_a)
+        disp("success" + newline);
+    else
+        disp("failure" + newline);
+    end
+
+    % Test #5 - 5 erasures
+    f_tilde = bits_to_llr(f);
+
+    erasures = [3, 4, 5, 6, 7];
+    for i = 1:length(erasures)
+        idx = erasures(i);
+        f_tilde(idx) = 0;
+    end
+
+    decoded_a = PUCCH_decoder(f_tilde, length(a), L, min_sum);
+    disp("Test #5 (5 erasures, burst):");
+    if isequal(a, decoded_a)
+        disp("success" + newline);
+    else
+        disp("failure" + newline);
     end
 end
